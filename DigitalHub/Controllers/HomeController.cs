e72b249f3d29600ef79f0a6fcc130947b2b55007
Hieu Vo Nguyen Thanh <thanhhieu5100@gmail.com>
2024-11-06 20:05:31 +0700

    Đổi tên 'TrangChu.cshtml' sang 'Index.cshtml'; đã xoá file 'About.cshtml' và 'Contact.cshtml'

--- DigitalHub/Controllers/HomeController.cs
+++ DigitalHub/Controllers/HomeController.cs
@@ -16,33 +16,6 @@ namespace DigitalHub.Controllers
         private DigitalHub_DBEntities db = new DigitalHub_DBEntities();
         public ActionResult Index()
         {
-            // Danh sách sản phẩm mẫu
-            var products = new List<Product>
-        {
-            new Product { ProductID = 1, NamePro = "Laptop gaming Acer Nitro 16 Phoenix AN16 41 R50Z", DecriptionPro = "Laptop gaming 16gb ram", Price = 31490000, DiscountPrice = 29990000, ImagePro = "https://storage.googleapis.com/a1aa/image/Dlk3VfxEQP3cFiRd4jEUQ1oIicvRcqjs9bmETIpDwkGeqWnTA.jpg" },
-            
-            // Thêm các sản phẩm khác
-        };
-
-            return View(products);
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult TrangChu()
-        {
             var products = db.Products.Include(p => p.Category1);
             return View(products.ToList());
         }
